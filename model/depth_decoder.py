8fb1046ca4fc8e9bd787ffaa0a7361b00a711424
sally20921 <sally20921@github.com>
2021-07-12 01:29:14 -0400

    gitignore

---
+++ model/depth_decoder.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+from collections import OrderedDict
+
+# sigmoids at the output and ELU nonlinearities elsewhere
+# 
