e031ea1336e8074a948cd1b04725f43e00c6b6ba
sally20921 <sally20921@github.com>
2021-08-11 18:23:07 -0400

    utils

--- utils/misc.py
+++ utils/misc.py
@@ -2,61 +2,106 @@ from utils.types import is_list, is_int
 
 def filter_dict(dictionary, keywords):
     '''
-    returns only the keywords that are part of a dictionary
+    returns only the keywords (as a list) that are part of a dictionary
 
-    Arguments:
-        dictionary: dict
-            dictionary for filtering
-        keywords: list or str
-            keywords that will be filtered
+    Parameter
+    ________
+    dictionary: dict
+        dictionary for filtering
+    keywords: list or str
+        keywords that will be filtered
 
-    returns:
-        keywords: list or str
-            list containing the keywords that are keys in dictionary
+    Returns
+    ________
+    keywords: list
+        list containing the keywords that are keys in dictionary
     '''
     return [key for key in keywords if key in dictionary]
+    # if keywords is a string, key is a list of characters that are in dictionary as key # var is a single value => a list containing single value 
 
-def make_list(var, n=None):
+
+def make_list(var, n=None): #optionally var can be a list, if that's the case, do nothing # if var is somehow not single value but has elements n, then do nothing even though there's a number in n
+
+    # only multiply by n if var is a single value
+    # var == single value or var == n values
     '''
-    wraps the input into a list, and optionally repeats it to be size n
+    wrap the input (var) into a list, and optionally repeat it to be size n
 
-    Arguments:
-        var: any
-            variable to be wrapped in a list
-        n: int
-            how much the wrapped variable will be repeated
+    Parameter
+    _________
+    var: Any 
+        Variable to be wrapped in a list
+    n: int
+        how much the wrapped variable will be repeated
 
-    returns:
-        var_list: list
-            list generated from var
+    Returns
+    _______
+    var_list: list
+        list generated from var
+    
     '''
     var = var if is_list(var) else [var]
     if n is None:
         return var
     else:
-        assert len(var) == 1 or len(var) == n 'wrong list length for make_List'
+        assert len(var) == 1 or len(var) == n, 'Wrong list length for make_list in utils.misc')
         return var * n if len(var) == 1 else var
-
 
 def same_shape(shape1, shape2):
     '''
-    check if two shapes are the same
-    
-    Arguments:
-        shape1: tuple
-            first shape
-        shape2: tuple
-            second shape
+    checks if two tuples' shapes have exactly same elements in a tuple
+    Parameter
+    _________
+    shape1: tuple
+        first shape
+    shape2: tuple
+        second shape
 
-    returns:
-        flag: bool
-            True if both shapes are the same (same length and dimensions)
+    Returns
+    _________
+    flag: bool
+        True if both shapes are the same (same length and dimension)
     '''
     if len(shape1) != len(shape2):
         return False
-    for i in range(len(shape1)):
-        if shape1[i] != shape2[i]:
+    for i, elem in enumerate(shape1):
+        if elem != shape2[i]:
             return False
-
     return True
+
+def parse_crop_borders(borders, shape):
+    '''
+    calculate the borders (parse the borders)  => for cropping.
+
+    Parameters
+    __________
+    borders: tuple
+        border input for parsing. can be one of the following forms:
+        (int, int, int, int): y, height, x, width
+        (int, int): y, x 
+        --> in this case: height = image_height-y, width = image_width-x
+
+    shape: tuple
+        image_shape: (image_height, image_width)
+            used to determine crop boundaries
+
+    Returns
+    _______         (x1, y1, x2, y2)
+    borders: tuple (left, top, right, bottom)
+        parsed borders for cropping
+    '''
+    # if borders is an empty tuple (no borders to crop), return the full image
+    if len(borders) == 0:
+        return 0, 0, shape[1], shape[0]
+
+    # copy borders as a list for modification
+    borders = list(borders).copy()
+    # copy() method returns a new list. 
+
+    # if the borders are 4-dimensional 
+    if len(borders) == 4:
+        borders = [borders[2], borders[0], borders[3], borders[1]]
+                # put it in (x1, y1, width, height) order
+
+
 
--- utils/types.py
+++ utils/types.py
@@ -1,57 +1,45 @@
-import yacs
 import numpy as np
 import torch
 
 def is_numpy(data):
-    '''
-    check if data is a numpy array
-    '''
+    # check if data is a numpy array
     return isinstance(data, np.ndarray)
 
 def is_tensor(data):
-    '''
-    check if data is a torch tensor
-    '''
-    return type(data) == torch.Tensor
+    # check if data is a torch tensor
+    return torch.is_tensor(data)
+    # this function is simply doing isinstance(obj, Tensor)
 
 def is_tuple(data):
-    '''
-    check if data is a tuple
-    '''
+    return isinstance(data, tuple)
+
+
+def is_tuple(data):
+    """Checks if data is a tuple."""
     return isinstance(data, tuple)
 
 def is_list(data):
-    '''
-    check if data is a list
-    '''
+    """Checks if data is a list."""
     return isinstance(data, list)
 
 def is_dict(data):
-    '''
-    check if data is a dictionary
-    '''
+    """Checks if data is a dictionary."""
     return isinstance(data, dict)
 
 def is_str(data):
-    '''
-    check if data is a string
-    '''
+    """Checks if data is a string."""
     return isinstance(data, str)
 
 def is_int(data):
-    '''
-    check if data is an integer
-    '''
+    """Checks if data is an integer."""
     return isinstance(data, int)
 
 def is_seq(data):
     '''
-    check if data is a list or tuple
+    checks if data is a list or tuple
     '''
     return is_tuple(data) or is_list(data)
 
-def is_cfg(data):
-    '''
-    check if data is a configuration node
-    '''
-    return type(data) == yacs.config.CfgNode
+
+
+
